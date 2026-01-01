721824e9b4bea9a84d23b7dec8d703f751809236
BD103 <59022059+BD103@users.noreply.github.com>
2026-01-01 11:23:55 -0500

    feat: warn if original permission level was not 3

--- src/main/java/io/github/bd103/ticklowerperm/mixin/TickCommandMixin.java
+++ src/main/java/io/github/bd103/ticklowerperm/mixin/TickCommandMixin.java
@@ -1,6 +1,9 @@
 package io.github.bd103.ticklowerperm.mixin;
 
 import net.minecraft.server.commands.TickCommand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -13,7 +16,12 @@ public abstract class TickCommandMixin {
         at = @At(value = "INVOKE", target = "hasPermission(I)Lnet/minecraft/server/commands/PermissionCheck;"),
         index = 0
     )
-    private static int permissionLevel(int _value) {
+    private static int permissionLevel(int value) {
+        if (value != 3) {
+            Logger logger = LoggerFactory.getLogger("ticklowerperm");
+            logger.warn("Expected /tick permission level to be 3, but it was " + value + " instead. Continuing to override it to 2.");
+        }
+
         return 2;
     }
 }
