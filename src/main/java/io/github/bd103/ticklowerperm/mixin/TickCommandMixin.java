082465c8051987ae653bfde2308db7cf9b04a4b6
BD103 <59022059+BD103@users.noreply.github.com>
2026-01-01 11:08:34 -0500

    chore: migrate to mojmap
    Closes #8

--- src/main/java/io/github/bd103/ticklowerperm/mixin/TickCommandMixin.java
+++ src/main/java/io/github/bd103/ticklowerperm/mixin/TickCommandMixin.java
@@ -1,6 +1,6 @@
 package io.github.bd103.ticklowerperm.mixin;
 
-import net.minecraft.server.command.TickCommand;
+import net.minecraft.server.commands.TickCommand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -10,7 +10,7 @@ public abstract class TickCommandMixin {
     // Replace the argument representing the permission level from 3 to 2, allowing command blocks to run it.
     @ModifyArg(
         method = "register(Lcom/mojang/brigadier/CommandDispatcher;)V",
-        at = @At(value = "INVOKE", target = "requirePermissionLevel(I)Lnet/minecraft/command/PermissionLevelPredicate;"),
+        at = @At(value = "INVOKE", target = "hasPermission(I)Lnet/minecraft/server/commands/PermissionCheck;"),
         index = 0
     )
     private static int permissionLevel(int _value) {
